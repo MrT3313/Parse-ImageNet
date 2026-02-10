54dd2a1d1fe2ec026f7fe31db2ae31abea66840b
Reed Turgeon <turgeon.dev@gmail.com>
2026-02-09 22:46:12 -0500

    V1.2.0 : get_synset_mapping(...) | wnid -> category names (#5)

--- parseimagenet/__init__.py
+++ parseimagenet/__init__.py
@@ -1,9 +1,9 @@
-from .ParseImageNetSubset import get_image_paths_by_keywords, get_available_presets, KEYWORD_PRESETS
+from .ParseImageNetSubset import get_image_paths_by_keywords, get_available_presets, get_synset_mapping, KEYWORD_PRESETS
 from .keywords.bird_breeds import bird_breeds
 from .keywords.dog_breeds import dog_breeds, wild_canid_breeds
 from .keywords.snake_breeds import snake_breeds
 
 __all__ = [
-    'get_image_paths_by_keywords', 'get_available_presets', 'KEYWORD_PRESETS',
+    'get_image_paths_by_keywords', 'get_available_presets', 'get_synset_mapping', 'KEYWORD_PRESETS',
     'bird_breeds', 'dog_breeds', 'wild_canid_breeds', 'snake_breeds'
 ]
